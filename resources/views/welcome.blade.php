aaa281f44b4be9f658f4926e24a47e078ac7c1b4
Teun <teun.vanhelvoort2@edu-kw1c.nl>
2020-09-09 13:19:54 +0200

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="{{mix("css/app.css")}}" rel="stylesheet">
+</head>
+<body>
+<h1 class="main">Welcome to my Site</h1>
+<hr>
+<p class="main">Hello and welcome to my website. <br>
+    I will be adding more content soon.</p>
+<p class="main">
+    please click on these links to go to other pages:
+</p>
+<ul class="main">
+    <li><a>Placeholder 1</a></li>
+    <br>
+    <li><a>Placeholder 2</a></li>
+    <br>
+    <li><a>Placeholder 3</a></li>
+    <br>
+    <li><a>Placeholder 4</a></li>
+    <br>
+    <li><a>Placeholder 5</a></li>
+    <br>
+</ul>
+<hr>
+<h2>Lorem Ipsum</h2>
+<div id="left">
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque gravida porta nisl.<br>
+        Ut bibendum accumsan nisl at ullamcorper. Etiam libero dolor, lobortis in vestibulum<br>
+        suscipit, euismod ac sem. Fusce molestie, ante in porta commodo, lectus felis pellentesque<br>
+        ligula, ut suscipit lorem libero sit amet elit. Donec eget sollicitudin elit, a mollis quam.<br>
+        Ut gravida, eros eu scelerisque porttitor, est tellus vulputate quam, non molestie ligula metus<br>
+        a augue. Aenean sodales lacus sit amet faucibus scelerisque. Sed commodo consequat ante quis mollis.<br>
+        Donec eget suscipit diam. Sed imperdiet, quam sit amet rhoncus scelerisque, neque nisi egestas quam,<br>
+        sagittis ultrices augue nibh id purus.
+    </p>
+</div>
+<div id="right">
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque gravida porta nisl.<br>
+        Ut bibendum accumsan nisl at ullamcorper. Etiam libero dolor, lobortis in vestibulum<br>
+        suscipit, euismod ac sem. Fusce molestie, ante in porta commodo, lectus felis pellentesque<br>
+        ligula, ut suscipit lorem libero sit amet elit. Donec eget sollicitudin elit, a mollis quam.<br>
+        Ut gravida, eros eu scelerisque porttitor, est tellus vulputate quam, non molestie ligula metus<br>
+        a augue. Aenean sodales lacus sit amet faucibus scelerisque. Sed commodo consequat ante quis mollis.<br>
+        Donec eget suscipit diam. Sed imperdiet, quam sit amet rhoncus scelerisque, neque nisi egestas quam,<br>
+        sagittis ultrices augue nibh id purus.
+    </p>
+</div>
+<hr>
+<p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque gravida porta nisl.<br>
+Ut bibendum accumsan nisl at ullamcorper. Etiam libero dolor, lobortis in vestibulum<br>
+suscipit, euismod ac sem. Fusce molestie, ante in porta commodo, lectus felis pellentesque<br>
+ligula, ut suscipit lorem libero sit amet elit. Donec eget sollicitudin elit, a mollis quam.<br>
+Ut gravida, eros eu scelerisque porttitor, est tellus vulputate quam, non molestie ligula metus<br>
+a augue. Aenean sodales lacus sit amet faucibus scelerisque. Sed commodo consequat ante quis mollis.<br>
+Donec eget suscipit diam. Sed imperdiet, quam sit amet rhoncus scelerisque, neque nisi egestas quam,<br>
+sagittis ultrices augue nibh id purus.
+</p>
+</body>
+</html>
